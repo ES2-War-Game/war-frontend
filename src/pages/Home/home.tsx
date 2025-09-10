75d0724f99a2720a4231e37d25434d96a35e18a7
Carlos Alberto <carlosjr0421@gmail.com>
2025-09-09 22:04:41 -0300

    feat:criando pagina inicial do jogo

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,18 @@
+import style from "./home.module.css"
+import logo from "../../assets/war_logo.png"
+import { Link } from "react-router-dom"
+
+
+export default function Home(){
+    return(
+        <div className={style.container}>
+            <img src={logo} alt="" />
+            <div className={style.buttons}>
+                <button><Link to="/jogadores">SinglePlayer</Link></button>
+                <button ><Link to="/jogadores">MultiPLayer</Link></button>
+                <button><Link to="/login">Login</Link></button>
+            </div>
+        </div>
+
+    )
+}
